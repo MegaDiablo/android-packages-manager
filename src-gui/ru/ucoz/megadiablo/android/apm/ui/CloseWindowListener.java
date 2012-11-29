d3a02e6d8bdf731294c82586c7da68349d2c0c75
vbaraznovsky <ginVavilon@gmail.com>
2012-11-29 16:05:46 +0300

    add CloseWindowListener.java

---
+++ src-gui/ru/ucoz/megadiablo/android/apm/ui/CloseWindowListener.java
@@ -0,0 +1,45 @@
+/**
+ *
+ */
+package ru.ucoz.megadiablo.android.apm.ui;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+/**
+ * Слушатель Для закрытия окон
+ *
+ * @author Vladimir Baraznovsky
+ *
+ */
+public abstract class CloseWindowListener implements WindowListener {
+
+	@Override
+	public void windowActivated(final WindowEvent pE) {
+	}
+
+	@Override
+	public void windowClosed(final WindowEvent pE) {
+
+	}
+
+	@Override
+	public void windowDeactivated(final WindowEvent pE) {
+
+	}
+
+	@Override
+	public void windowDeiconified(final WindowEvent pE) {
+
+	}
+
+	@Override
+	public void windowIconified(final WindowEvent pE) {
+	}
+
+	@Override
+	public void windowOpened(final WindowEvent pE) {
+
+	}
+
+}

170c7fb31a63434fdaa9a321e5c366978d2fd3db
vbaraznovsky <ginVavilon@gmail.com>
2013-09-05 12:58:00 +0300

    add AdbShell

---
+++ src-core/com/adbhelper/adb/shell/AdbShell.java
@@ -0,0 +1,39 @@
+package com.adbhelper.adb.shell;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ *
+ * @author Vladimir Baraznovsky
+ *
+ */
+public class AdbShell {
+	private final Process mProcess;
+
+	private AdbShell(final Process pProcess) {
+		super();
+		mProcess = pProcess;
+	}
+
+	public Process getProcess() {
+		return mProcess;
+	}
+
+	public void destroy() {
+		mProcess.destroy();
+	}
+
+	public InputStream getErrorStream() {
+		return mProcess.getErrorStream();
+	}
+
+	public InputStream getInputStream() {
+		return mProcess.getInputStream();
+	}
+
+	public OutputStream getOutputStream() {
+		return mProcess.getOutputStream();
+	}
+
+}

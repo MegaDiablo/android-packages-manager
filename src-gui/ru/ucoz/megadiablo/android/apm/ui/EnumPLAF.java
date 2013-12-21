0254043ffe862b8fa5d2f4807e96956bb7429ec7
Alexander Gromyko <megadiablogal@gmail.com>
2013-12-21 10:37:12 +0300

    Удалил из исходников библиотеку JTatoo. Теперь она будет поставляться как отдельная библиотека.

--- src-gui/ru/ucoz/megadiablo/android/apm/ui/EnumPLAF.java
+++ src-gui/ru/ucoz/megadiablo/android/apm/ui/EnumPLAF.java
@@ -7,133 +7,133 @@ import javax.swing.LookAndFeel;
  * */
 public enum EnumPLAF {
 
-	AERO("Aero", null) {
-		private final LookAndFeel LOOK_AND_FEEL =
-				new com.jtattoo.plaf.aero.AeroLookAndFeel();
-
-		@Override
-		public LookAndFeel getPLookAndFeel() {
-			return LOOK_AND_FEEL;
-		}
-
-		@Override
-		public void setThemeDefault() {
-			com.jtattoo.plaf.aero.AeroLookAndFeel.setTheme(DEFAULT);
-		}
-	},
-	HIFI("HiFi", null) {
-		private final LookAndFeel LOOK_AND_FEEL =
-				new com.jtattoo.plaf.hifi.HiFiLookAndFeel();
-
-		@Override
-		public LookAndFeel getPLookAndFeel() {
-			return LOOK_AND_FEEL;
-		}
-
-		@Override
-		public void setThemeDefault() {
-			com.jtattoo.plaf.hifi.HiFiLookAndFeel.setTheme(DEFAULT);
-		}
-	},
-	ALUMINIUM("Aluminium", null) {
-		private final LookAndFeel LOOK_AND_FEEL =
-				new com.jtattoo.plaf.aluminium.AluminiumLookAndFeel();
-
-		@Override
-		public LookAndFeel getPLookAndFeel() {
-			return LOOK_AND_FEEL;
-		}
-
-		@Override
-		public void setThemeDefault() {
-			com.jtattoo.plaf.aluminium.AluminiumLookAndFeel.setTheme(DEFAULT);
-		}
-	},
-	FAST("Fast", null) {
-		private final LookAndFeel LOOK_AND_FEEL =
-				new com.jtattoo.plaf.fast.FastLookAndFeel();
-
-		@Override
-		public LookAndFeel getPLookAndFeel() {
-			return LOOK_AND_FEEL;
-		}
-
-		@Override
-		public void setThemeDefault() {
-			com.jtattoo.plaf.fast.FastLookAndFeel.setTheme(DEFAULT);
-		}
-	},
-	MCWIN("McWin", null) {
-		private final LookAndFeel LOOK_AND_FEEL =
-				new com.jtattoo.plaf.mcwin.McWinLookAndFeel();
-
-		@Override
-		public LookAndFeel getPLookAndFeel() {
-			return LOOK_AND_FEEL;
-		}
-
-		@Override
-		public void setThemeDefault() {
-			com.jtattoo.plaf.mcwin.McWinLookAndFeel.setTheme(DEFAULT);
-		}
-	},
-	MINT("Mint", null) {
-		private final LookAndFeel LOOK_AND_FEEL =
-				new com.jtattoo.plaf.mint.MintLookAndFeel();
-
-		@Override
-		public LookAndFeel getPLookAndFeel() {
-			return LOOK_AND_FEEL;
-		}
-
-		@Override
-		public void setThemeDefault() {
-			com.jtattoo.plaf.mint.MintLookAndFeel.setTheme(DEFAULT);
-		}
-	},
-	NOIRE("Noire", null) {
-		private final LookAndFeel LOOK_AND_FEEL =
-				new com.jtattoo.plaf.noire.NoireLookAndFeel();
-
-		@Override
-		public LookAndFeel getPLookAndFeel() {
-			return LOOK_AND_FEEL;
-		}
-
-		@Override
-		public void setThemeDefault() {
-			com.jtattoo.plaf.noire.NoireLookAndFeel.setTheme(DEFAULT);
-		}
-	},
-	SMART("Smart", null) {
-		private final LookAndFeel LOOK_AND_FEEL =
-				new com.jtattoo.plaf.smart.SmartLookAndFeel();
-
-		@Override
-		public LookAndFeel getPLookAndFeel() {
-			return LOOK_AND_FEEL;
-		}
-
-		@Override
-		public void setThemeDefault() {
-			com.jtattoo.plaf.smart.SmartLookAndFeel.setTheme(DEFAULT);
-		}
-	},
-	ACRY("Acry", null) {
-		private final LookAndFeel LOOK_AND_FEEL =
-				new com.jtattoo.plaf.acryl.AcrylLookAndFeel();
-
-		@Override
-		public LookAndFeel getPLookAndFeel() {
-			return LOOK_AND_FEEL;
-		}
-
-		@Override
-		public void setThemeDefault() {
-			com.jtattoo.plaf.acryl.AcrylLookAndFeel.setTheme(DEFAULT);
-		}
-	},
-	METAL("Metal", null) {
+//	AERO("Aero", null) {
+//		private final LookAndFeel LOOK_AND_FEEL =
+//				new com.jtattoo.plaf.aero.AeroLookAndFeel();
+//
+//		@Override
+//		public LookAndFeel getPLookAndFeel() {
+//			return LOOK_AND_FEEL;
+//		}
+//
+//		@Override
+//		public void setThemeDefault() {
+//			com.jtattoo.plaf.aero.AeroLookAndFeel.setTheme(DEFAULT);
+//		}
+//	},
+//	HIFI("HiFi", null) {
+//		private final LookAndFeel LOOK_AND_FEEL =
+//				new com.jtattoo.plaf.hifi.HiFiLookAndFeel();
+//
+//		@Override
+//		public LookAndFeel getPLookAndFeel() {
+//			return LOOK_AND_FEEL;
+//		}
+//
+//		@Override
+//		public void setThemeDefault() {
+//			com.jtattoo.plaf.hifi.HiFiLookAndFeel.setTheme(DEFAULT);
+//		}
+//	},
+//	ALUMINIUM("Aluminium", null) {
+//		private final LookAndFeel LOOK_AND_FEEL =
+//				new com.jtattoo.plaf.aluminium.AluminiumLookAndFeel();
+//
+//		@Override
+//		public LookAndFeel getPLookAndFeel() {
+//			return LOOK_AND_FEEL;
+//		}
+//
+//		@Override
+//		public void setThemeDefault() {
+//			com.jtattoo.plaf.aluminium.AluminiumLookAndFeel.setTheme(DEFAULT);
+//		}
+//	},
+//	FAST("Fast", null) {
+//		private final LookAndFeel LOOK_AND_FEEL =
+//				new com.jtattoo.plaf.fast.FastLookAndFeel();
+//
+//		@Override
+//		public LookAndFeel getPLookAndFeel() {
+//			return LOOK_AND_FEEL;
+//		}
+//
+//		@Override
+//		public void setThemeDefault() {
+//			com.jtattoo.plaf.fast.FastLookAndFeel.setTheme(DEFAULT);
+//		}
+//	},
+//	MCWIN("McWin", null) {
+//		private final LookAndFeel LOOK_AND_FEEL =
+//				new com.jtattoo.plaf.mcwin.McWinLookAndFeel();
+//
+//		@Override
+//		public LookAndFeel getPLookAndFeel() {
+//			return LOOK_AND_FEEL;
+//		}
+//
+//		@Override
+//		public void setThemeDefault() {
+//			com.jtattoo.plaf.mcwin.McWinLookAndFeel.setTheme(DEFAULT);
+//		}
+//	},
+//	MINT("Mint", null) {
+//		private final LookAndFeel LOOK_AND_FEEL =
+//				new com.jtattoo.plaf.mint.MintLookAndFeel();
+//
+//		@Override
+//		public LookAndFeel getPLookAndFeel() {
+//			return LOOK_AND_FEEL;
+//		}
+//
+//		@Override
+//		public void setThemeDefault() {
+//			com.jtattoo.plaf.mint.MintLookAndFeel.setTheme(DEFAULT);
+//		}
+//	},
+//	NOIRE("Noire", null) {
+//		private final LookAndFeel LOOK_AND_FEEL =
+//				new com.jtattoo.plaf.noire.NoireLookAndFeel();
+//
+//		@Override
+//		public LookAndFeel getPLookAndFeel() {
+//			return LOOK_AND_FEEL;
+//		}
+//
+//		@Override
+//		public void setThemeDefault() {
+//			com.jtattoo.plaf.noire.NoireLookAndFeel.setTheme(DEFAULT);
+//		}
+//	},
+//	SMART("Smart", null) {
+//		private final LookAndFeel LOOK_AND_FEEL =
+//				new com.jtattoo.plaf.smart.SmartLookAndFeel();
+//
+//		@Override
+//		public LookAndFeel getPLookAndFeel() {
+//			return LOOK_AND_FEEL;
+//		}
+//
+//		@Override
+//		public void setThemeDefault() {
+//			com.jtattoo.plaf.smart.SmartLookAndFeel.setTheme(DEFAULT);
+//		}
+//	},
+//	ACRY("Acry", null) {
+//		private final LookAndFeel LOOK_AND_FEEL =
+//				new com.jtattoo.plaf.acryl.AcrylLookAndFeel();
+//
+//		@Override
+//		public LookAndFeel getPLookAndFeel() {
+//			return LOOK_AND_FEEL;
+//		}
+//
+//		@Override
+//		public void setThemeDefault() {
+//			com.jtattoo.plaf.acryl.AcrylLookAndFeel.setTheme(DEFAULT);
+//		}
+//	},
+	AERO("Metal", null) {
 		private final LookAndFeel LOOK_AND_FEEL =
 				new javax.swing.plaf.metal.MetalLookAndFeel();
 
